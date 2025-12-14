3ea2854d0306b5ab273c4f898d2d3a7baab86136
zenzer0s <praveenzalaki200@gmail.com>
2025-12-14 11:42:33 +0530

    alioth: prop: Move props to vendor

--- device.mk
+++ device.mk
@@ -237,13 +237,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.display.sensortype=2 \
     vendor.display.qdcm.mode_combine=1 \
     vendor.display.defer_fps_frame_count=2
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=2000 \
-    ro.surface_flinger.set_touch_timer_ms=2000 \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true
-
+   
 PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
     vendor.display.config@1.9.vendor \
