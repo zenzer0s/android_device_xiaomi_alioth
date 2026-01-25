d5cfa8a4a7511e28c8f6a90a943a044bae6283f0
zenzer0s <praveenzalaki200@gmail.com>
2026-01-25 18:01:19 +0000

    Revert "alioth: add true/false flag for revanced"
    This reverts commit 6112e61e192d9f5e81b628db3ab671cd7d9c3c1d.

--- parts/src/org/lineageos/xiaomiparts/services/BootCompletedReceiver.kt
+++ parts/src/org/lineageos/xiaomiparts/services/BootCompletedReceiver.kt
@@ -3,11 +3,7 @@ package org.lineageos.xiaomiparts.services
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.ComponentName
-import android.content.pm.PackageManager
-import android.os.SystemProperties
 import androidx.preference.PreferenceManager
-import org.lineageos.xiaomiparts.ui.revanced.ReVancedActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -27,17 +23,6 @@ class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Logging.d(TAG, "Received intent: ${intent.action}")
         if (intent.action != Intent.ACTION_LOCKED_BOOT_COMPLETED) return
-
-        val isReVancedAvailable = SystemProperties.getBoolean(PROPERTY_REVANCED_AVAILABLE, false)
-        val pm = context.packageManager
-        val componentName = ComponentName(context, ReVancedActivity::class.java)
-        
-        pm.setComponentEnabledSetting(
-            componentName,
-            if (isReVancedAvailable) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-            else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-            PackageManager.DONT_KILL_APP
-        )
 
         Logging.i(TAG, "Boot completed, restoring settings...")
         
