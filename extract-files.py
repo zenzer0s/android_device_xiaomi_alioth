c9a246db6cf28668aca603b02aa75ab1270d4252
binbin323 <bin@dhao.cc>
2026-06-24 20:21:40 +0900

    alioth: Patch libwfdmmsrc_system with libgui_shim

--- extract-files.py
+++ extract-files.py
@@ -71,6 +71,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'writepid\s+/dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh HighPerformance'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
 }  # fmt: skip
 
 
