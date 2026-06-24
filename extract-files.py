ccb53d9bbe9cdef57063b415d753d01497096afa
ralph950412 <ralph950412@gmail.com>
2026-06-24 20:12:31 +0900

    alioth: Patch libwfdservice to load android.media.audio.common.types-V5-cpp

--- extract-files.py
+++ extract-files.py
@@ -69,6 +69,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc': blob_fixup()
         .regex_replace(r'writepid\s+/dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh HighPerformance'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
 }  # fmt: skip
 
 
